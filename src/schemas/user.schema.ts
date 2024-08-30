84e3ad5d69117160b1caacd4573ab102529fb367
AndreLuisPLuz <andreluispluz@gmail.com>
2024-08-30 09:05:56 -0300

    feat: user schema created.

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,36 @@
+import { HydratedDocument, Model, Schema } from "mongoose";
+
+interface IUser {
+    name: string,
+    email: string,
+    password: string
+};
+
+interface IUserMethods {
+    trySave(): Promise<{ savedUser: HydratedDocument<IUser, IUserMethods>, success: boolean }>;
+}
+
+type UserModel = Model<IUser, {}, IUserMethods>;
+
+const userSchema = new Schema<IUser, UserModel, IUserMethods>({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+userSchema.method(
+    "trySave",
+    async function trySave() {
+        const savedUser = await this.save();
+        const success = (savedUser != null);
+
+        return { savedUser, success };
+    }
+);
+
+export {
+    IUser,
+    IUserMethods,
+    UserModel,
+    userSchema
+};
